d7c2bb50eeae249f6648b479d309fb888c516d1d
Lenovo <@>
2024-03-20 20:06:22 +0300

    свой шаблон

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+app.app_context().push()
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     price = db.Column(db.Float, nullable=False)
-    image = db.Column(db.LargeBinary, nullable=False)
-    isActive = db.Column(db.Boolean, default=False)
-    publication_date = db.Column(db.DateTime, default=datetime.now())
+    isActive = db.Column(db.Boolean, default=True)
+    # image = db.Column(db.LargeBinary, nullable=False)
+    # publication_date = db.Column(db.DateTime, default=datetime.now())
+    # text = db.Column(db.Text, nullable=False)
 
 
 @app.route('/')
@@ -24,6 +26,12 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
 
 
 @app.route('/new')
